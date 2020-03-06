a2b6675d4919ce702fbe151105412d821aafbdae
ganymed <git@dankito.net>
2020-03-06 01:25:17 +0100

    Added listFiles()

--- FilesystemWalker/src/main/kotlin/net/dankito/documents/search/filesystem/FilesystemWalker.kt
+++ FilesystemWalker/src/main/kotlin/net/dankito/documents/search/filesystem/FilesystemWalker.kt
@@ -15,6 +15,23 @@ import java.nio.file.attribute.BasicFileAttributes
 
 open class FilesystemWalker {
 
+	open fun listFiles(startDir: Path): List<Path> {
+		val discoveredFiles = mutableListOf<Path>()
+
+		Files.walkFileTree(startDir, object : SimpleFileVisitor<Path>() {
+			override fun visitFile(file: Path?, attributes: BasicFileAttributes?): FileVisitResult {
+				file?.let {
+					discoveredFiles.add(file)
+				}
+
+				return FileVisitResult.CONTINUE
+			}
+		})
+
+		return discoveredFiles
+	}
+
+
 	open fun walk(startDir: Path, discoveredFilesQueue: AsyncProducerConsumerQueue<Path>) {
 
 		Files.walkFileTree(startDir, object : SimpleFileVisitor<Path>() {

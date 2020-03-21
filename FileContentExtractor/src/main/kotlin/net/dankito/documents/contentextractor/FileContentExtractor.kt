066d4939d054d8d6316869b41ff5cd05f4fe5c8a
dankl <dankito@users.noreply.github.com>
2020-03-21 18:36:18 +0100

    Added ImageBasedPdfTextExtractor, Tika therefore should do no OCR on PDF files anymore

--- FileContentExtractor/src/main/kotlin/net/dankito/documents/contentextractor/FileContentExtractor.kt
+++ FileContentExtractor/src/main/kotlin/net/dankito/documents/contentextractor/FileContentExtractor.kt
@@ -4,13 +4,15 @@ import net.dankito.documents.contentextractor.model.FileContentExtractorSettings
 import net.dankito.text.extraction.ITextExtractorRegistry
 import net.dankito.text.extraction.TextExtractorRegistry
 import net.dankito.text.extraction.TikaTextExtractor
-import net.dankito.text.extraction.image.Tesseract4ImageTextExtractor
+import net.dankito.text.extraction.image.Tesseract4CommandlineImageTextExtractor
 import net.dankito.text.extraction.image.model.OcrOutputType
 import net.dankito.text.extraction.image.model.TesseractConfig
 import net.dankito.text.extraction.model.PdfContentExtractorStrategy
 import net.dankito.text.extraction.model.TikaSettings
+import net.dankito.text.extraction.pdf.ImageBasedPdfTextExtractor
 import net.dankito.text.extraction.pdf.OpenPdfPdfTextExtractor
 import net.dankito.text.extraction.pdf.pdfToTextPdfTextExtractor
+import net.dankito.text.extraction.pdf.pdfimagesImagesFromPdfExtractor
 import java.io.File
 
 
@@ -25,13 +27,15 @@ open class FileContentExtractor(protected val settings: FileContentExtractorSett
 
 
 	protected open fun initTextExtractorRegistry(): ITextExtractorRegistry {
+		val tesseract4CommandlineImageTextExtractor = Tesseract4CommandlineImageTextExtractor(TesseractConfig(settings.ocrLanguages, OcrOutputType.Text,
+				settings.tesseractPath, settings.tessdataDirectory))
+
 		return TextExtractorRegistry(listOf(
 				pdfToTextPdfTextExtractor(),
 				OpenPdfPdfTextExtractor(),
-				Tesseract4ImageTextExtractor(TesseractConfig(settings.ocrLanguages, OcrOutputType.Text,
-						settings.tesseractPath, settings.tessdataDirectory)),
-				TikaTextExtractor(TikaSettings(PdfContentExtractorStrategy.OcrAndText, settings.ocrLanguages,
-						OcrOutputType.Text, settings.tesseractPath, settings.tessdataDirectory))
+				tesseract4CommandlineImageTextExtractor,
+				ImageBasedPdfTextExtractor(tesseract4CommandlineImageTextExtractor, pdfimagesImagesFromPdfExtractor()),
+				TikaTextExtractor(TikaSettings(PdfContentExtractorStrategy.NoOcr))
 		))
 	}
 

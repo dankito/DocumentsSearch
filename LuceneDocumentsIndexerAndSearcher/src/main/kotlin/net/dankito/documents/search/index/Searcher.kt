7067ded0baf0ee9bae232ba3d03ee255f5a1f395
dankl <dankito@users.noreply.github.com>
2020-03-22 21:43:27 +0100

    Raised default value of countMaxResults to 10000

--- LuceneDocumentsIndexerAndSearcher/src/main/kotlin/net/dankito/documents/search/index/Searcher.kt
+++ LuceneDocumentsIndexerAndSearcher/src/main/kotlin/net/dankito/documents/search/index/Searcher.kt
@@ -11,13 +11,13 @@ import org.apache.lucene.store.Directory
 open class Searcher {
 
 	@JvmOverloads
-	open fun search(directory: Directory, query: Query, countMaxResults: Int = 1000,
+	open fun search(directory: Directory, query: Query, countMaxResults: Int = 10_000,
 					sortFields: List<SortField> = listOf()): SearchResults {
 		val reader = DirectoryReader.open(directory)
 		val searcher = IndexSearcher(reader)
 
 		val topDocs = if (sortFields.isEmpty()) searcher.search(query, countMaxResults)
-						else searcher.search(query, countMaxResults, Sort(*sortFields.toTypedArray()))
+					  else searcher.search(query, countMaxResults, Sort(*sortFields.toTypedArray()))
 
 		val hits = topDocs.scoreDocs.map { SearchResult(it.score, searcher.doc(it.doc)) }
 

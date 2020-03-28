99f89c59f93c8170eb0f6a72b268b43ceede893c
dankl <dankito@users.noreply.github.com>
2020-03-28 17:07:50 +0100

    Added nullable overloads

--- LuceneDocumentsIndexerAndSearcher/src/main/kotlin/net/dankito/documents/search/index/FieldMapper.kt
+++ LuceneDocumentsIndexerAndSearcher/src/main/kotlin/net/dankito/documents/search/index/FieldMapper.kt
@@ -1,7 +1,7 @@
 package net.dankito.documents.search.index
 
 import org.apache.lucene.document.Document
-import java.util.Date
+import java.util.*
 
 
 open class FieldMapper {
@@ -10,24 +10,60 @@ open class FieldMapper {
 		return document.getField(fieldName).stringValue()
 	}
 
+	open fun nullableString(document: Document, fieldName: String): String? {
+		return document.getField(fieldName)?.stringValue()
+	}
+
 	open fun int(document: Document, fieldName: String): Int {
-		return document.getField(fieldName).numericValue().toInt()
+		return number(document, fieldName).toInt()
+	}
+
+	open fun nullableInt(document: Document, fieldName: String): Int? {
+		return nullableNumber(document, fieldName)?.toInt()
 	}
 
 	open fun long(document: Document, fieldName: String): Long {
-		return document.getField(fieldName).numericValue().toLong()
+		return number(document, fieldName).toLong()
+	}
+
+	open fun nullableLong(document: Document, fieldName: String): Long? {
+		return nullableNumber(document, fieldName)?.toLong()
 	}
 
 	open fun float(document: Document, fieldName: String): Float {
-		return document.getField(fieldName).numericValue().toFloat()
+		return number(document, fieldName).toFloat()
+	}
+
+	open fun nullableFloat(document: Document, fieldName: String): Float? {
+		return nullableNumber(document, fieldName)?.toFloat()
 	}
 
 	open fun double(document: Document, fieldName: String): Double {
-		return document.getField(fieldName).numericValue().toDouble()
+		return number(document, fieldName).toDouble()
+	}
+
+	open fun nullableDouble(document: Document, fieldName: String): Double? {
+		return nullableNumber(document, fieldName)?.toDouble()
 	}
 
 	open fun date(document: Document, fieldName: String): Date {
-		return Date(document.getField(fieldName).numericValue().toLong())
+		return Date(long(document, fieldName))
+	}
+
+	open fun nullableDate(document: Document, fieldName: String): Date? {
+		nullableLong(document, fieldName)?.let {
+			return Date(it)
+		}
+
+		return null
+	}
+
+	open fun number(document: Document, fieldName: String): Number {
+		return document.getField(fieldName).numericValue()
+	}
+
+	open fun nullableNumber(document: Document, fieldName: String): Number? {
+		return document.getField(fieldName)?.numericValue()
 	}
 
 }

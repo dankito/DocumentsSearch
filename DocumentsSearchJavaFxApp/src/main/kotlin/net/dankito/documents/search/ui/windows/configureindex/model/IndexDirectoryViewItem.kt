a4d54a5f413e46a261dca6036527362cd2f25916
dankl <dankito@users.noreply.github.com>
2020-04-04 01:28:06 +0200

    Implemented determining asynchronously and off UI thread count files in index directory

--- DocumentsSearchJavaFxApp/src/main/kotlin/net/dankito/documents/search/ui/windows/configureindex/model/IndexDirectoryViewItem.kt
+++ DocumentsSearchJavaFxApp/src/main/kotlin/net/dankito/documents/search/ui/windows/configureindex/model/IndexDirectoryViewItem.kt
@@ -2,16 +2,46 @@ package net.dankito.documents.search.ui.windows.configureindex.model
 
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.javafx.JavaFx
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import net.dankito.documents.search.filesystem.FilesystemWalker
+import org.slf4j.LoggerFactory
 import tornadofx.*
 import java.io.File
 
 
 class IndexDirectoryViewItem(indexDirectory: File) : ItemViewModel<File>(indexDirectory) {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(IndexDirectoryViewItem::class.java)
+    }
+
+
+    init {
+        itemProperty.addListener { _, _, newValue -> updateCountFiles(newValue) }
+
+        updateCountFiles(indexDirectory)
+    }
+
+
     val path = bind(File::getAbsolutePath) as SimpleStringProperty
 
-    // TODO: determine count files asynchronously to not block UI thread
-    val countFiles = SimpleIntegerProperty(FilesystemWalker().listFiles(item.toPath()).size) // TODO: implement binding to update value when item changes
+    val countFiles = SimpleIntegerProperty(-1)
+
+
+    private fun updateCountFiles(indexDirectory: File) = GlobalScope.launch {
+        try {
+            val countFilesInDirectory = FilesystemWalker().listFiles(indexDirectory.toPath()).size
+
+            withContext(Dispatchers.JavaFx) {
+                countFiles.value = countFilesInDirectory
+            }
+        } catch (e: Exception) {
+            logger.error("Could not get count files in directory '$indexDirectory'", e)
+        }
+    }
 
 }

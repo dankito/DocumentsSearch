2fa58e8439cfa17c0006905de657dce56757e75c
dankl <dankito@users.noreply.github.com>
2020-04-04 01:05:05 +0200

    Implemented showing file metadata if available

--- DocumentsSearchJavaFxApp/src/main/kotlin/net/dankito/documents/search/ui/windows/mainwindow/model/DocumentMetadataListCellFragment.kt
+++ DocumentsSearchJavaFxApp/src/main/kotlin/net/dankito/documents/search/ui/windows/mainwindow/model/DocumentMetadataListCellFragment.kt
@@ -2,6 +2,7 @@ package net.dankito.documents.search.ui.windows.mainwindow.model
 
 import javafx.geometry.Pos
 import net.dankito.documents.search.model.DocumentMetadata
+import net.dankito.utils.javafx.ui.extensions.ensureOnlyUsesSpaceIfVisible
 import tornadofx.*
 
 
@@ -16,7 +17,28 @@ class DocumentMetadataListCellFragment : ListCellFragment<DocumentMetadata>() {
 		vbox {
 			alignment = Pos.CENTER_LEFT
 
-			label(document.filename) {
+			hbox {
+				alignment = Pos.CENTER_LEFT
+
+				label(document.metadata) {
+					visibleWhen(document.showMetadata)
+
+					ensureOnlyUsesSpaceIfVisible()
+				}
+
+				label(":") {
+					visibleWhen(document.showMetadata)
+
+					ensureOnlyUsesSpaceIfVisible()
+
+					hboxConstraints {
+						marginLeft = 0.0
+						marginRight = 8.0
+					}
+				}
+
+				label(document.filename)
+
 				vboxConstraints {
 					marginBottom = 8.0
 				}
--- DocumentsSearchJavaFxApp/src/main/kotlin/net/dankito/documents/search/ui/windows/mainwindow/model/DocumentMetadataViewModel.kt
+++ DocumentsSearchJavaFxApp/src/main/kotlin/net/dankito/documents/search/ui/windows/mainwindow/model/DocumentMetadataViewModel.kt
@@ -1,13 +1,51 @@
 package net.dankito.documents.search.ui.windows.mainwindow.model
 
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleStringProperty
 import net.dankito.documents.search.model.DocumentMetadata
 import tornadofx.*
 
 
 class DocumentMetadataViewModel : ItemViewModel<DocumentMetadata>() {
 
+	init {
+	    itemProperty.addListener { _, _, _ -> updateMetadata() }
+	}
+
+
 	val filename = bind(DocumentMetadata::filename)
 
+	val showMetadata = SimpleBooleanProperty(false)
+
+	val metadata = SimpleStringProperty("")
+
 	val url = bind(DocumentMetadata::url)
+
+
+	private fun updateMetadata() {
+		metadata.value = getMetadataString()
+
+		showMetadata.value = metadata.value.isNullOrBlank() == false
+	}
+
+	private fun getMetadataString(): String {
+		item?.let { item ->
+			var metadata = if (item.author != null && item.title != null) {
+				"${item.author} - ${item.title} "
+			}
+			else if (item.title != null) {
+				"${item.title} "
+			}
+			else ""
+
+			if (item.series != null) {
+				metadata += "(${item.series})"
+			}
+
+			return metadata
+		}
+
+		return ""
+	}
 
 }

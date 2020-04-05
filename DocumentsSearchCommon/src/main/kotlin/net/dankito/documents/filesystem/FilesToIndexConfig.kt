226429ee408c9fb0a9b083f508c494e8f63adfc8
dankl <dankito@users.noreply.github.com>
2020-04-05 22:44:31 +0200

    Implemented stopping traversal

--- DocumentsSearchCommon/src/main/kotlin/net/dankito/documents/filesystem/FilesToIndexConfig.kt
+++ DocumentsSearchCommon/src/main/kotlin/net/dankito/documents/filesystem/FilesToIndexConfig.kt
@@ -1,15 +1,17 @@
 package net.dankito.documents.filesystem
 
 import java.io.File
+import java.util.concurrent.atomic.AtomicBoolean
 
 
-open class FilesToIndexConfig(
+open class FilesToIndexConfig constructor(
         val indexDirectory: File,
         val includeFilesRegexPatterns: List<String> = listOf(),
         val excludeFilesRegexPatterns: List<String> = listOf(),
         val abortOnError: Boolean = false,
         val ignoreFilesLargerThanCountBytes: Long? = null,
-        val ignoreFilesSmallerThanCountBytes: Long? = null
+        val ignoreFilesSmallerThanCountBytes: Long? = null,
+        val stopTraversal: AtomicBoolean = AtomicBoolean(false)
 ) {
 
     val includeFilesRegEx: List<Regex> = includeFilesRegexPatterns.map { createRegex(it) }
--- DocumentsSearchCommon/src/main/kotlin/net/dankito/documents/filesystem/FilesToIndexFinder.kt
+++ DocumentsSearchCommon/src/main/kotlin/net/dankito/documents/filesystem/FilesToIndexFinder.kt
@@ -2,7 +2,10 @@ package net.dankito.documents.filesystem
 
 import net.dankito.utils.filesystem.FilesystemWalker
 import net.dankito.utils.filesystem.IFilesystemWalker
+import net.dankito.utils.filesystem.VisitedFile
 import java.io.File
+import java.nio.file.DirectoryStream
+import java.nio.file.FileSystem
 import java.nio.file.FileVisitResult
 import java.nio.file.Path
 import java.nio.file.attribute.BasicFileAttributes
@@ -21,32 +24,46 @@ open class FilesToIndexFinder(protected val filesystemWalker: IFilesystemWalker 
 
     open fun findFilesToIndex(config: FilesToIndexConfig, excludedFiles: ((ExcludedFile) -> Unit)? = null, filesToIndex: (Path) -> Unit) {
         filesystemWalker.detailedWalk(config.indexDirectory.toPath(), config.abortOnError,
-                { directory -> checkIfDirectoryShouldBeIgnored(directory, config, excludedFiles) }, null) { visitedFile ->
+                { directory -> checkIfDirectoryShouldBeIgnored(directory, config, excludedFiles, filesToIndex) }, null) { visitedFile ->
 
-            visitedFile.path?.let { file ->
-                if (visitedFile.visitSuccessful == false) {
-                    excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.Error))
-                }
-                else if (includeFile(file, config)) { // include overrules all other settings
-                    filesToIndex(file)
-                }
-                else if (excludeFile(file, config)) {
-                    excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.ExcludePatternMatches))
-                }
-                else if (fileIsLargerThanMaxFileSize(visitedFile.attributes, config)) {
-                    excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.FileLargerThanMaxFileSize))
-                }
-                else if (fileIsSmallerThanMinFileSize(visitedFile.attributes, config)) {
-                    excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.FileSmallerThanMinFileSize))
-                }
-                else {
-                    filesToIndex(file)
-                }
+            if (config.stopTraversal.get()) {
+                return@detailedWalk FileVisitResult.TERMINATE
+            }
+
+            checkIfFileShouldBeIncludedOrExcluded(visitedFile, config, excludedFiles, filesToIndex)
+
+            FileVisitResult.CONTINUE
+        }
+    }
+
+    private fun checkIfFileShouldBeIncludedOrExcluded(visitedFile: VisitedFile, config: FilesToIndexConfig, excludedFiles: ((ExcludedFile) -> Unit)?, filesToIndex: (Path) -> Unit) {
+        visitedFile.path?.let { file ->
+            if (visitedFile.visitSuccessful == false) {
+                excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.ErrorOccurred))
+            }
+            else if (includeFile(file, config)) { // include overrules all other settings
+                filesToIndex(file)
+            }
+            else if (excludeFile(file, config)) {
+                excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.ExcludePatternMatches))
+            }
+            else if (fileIsLargerThanMaxFileSize(visitedFile.attributes, config)) {
+                excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.FileLargerThanMaxFileSize))
+            }
+            else if (fileIsSmallerThanMinFileSize(visitedFile.attributes, config)) {
+                excludedFiles?.invoke(ExcludedFile(file, ExcludeReason.FileSmallerThanMinFileSize))
+            }
+            else {
+                filesToIndex(file)
             }
         }
     }
 
-    protected open fun checkIfDirectoryShouldBeIgnored(directory: Path?, config: FilesToIndexConfig, ignoredFiles: ((ExcludedFile) -> Unit)?): FileVisitResult {
+    protected open fun checkIfDirectoryShouldBeIgnored(directory: Path?, config: FilesToIndexConfig, ignoredFiles: ((ExcludedFile) -> Unit)?, filesToIndex: (Path) -> Unit): FileVisitResult {
+        if (config.stopTraversal.get()) {
+            return FileVisitResult.TERMINATE
+        }
+
         directory?.let {
             if (excludeAppliesButIncludeDoesNot(directory, config)) {
                 ignoredFiles?.let {
